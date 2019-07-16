3c06e425918cd2ec15fcebbeaa04bc93e0151e50
João Luiz Castro Naufel <joaoluiznaufel@gmail.com>
2019-07-16 14:36:50 +0200

    add access key at the instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my-instance" {
 
   ami             = "${data.aws_ami.server_name.id}"
-
   instance_type   = "t3.small"
+  key_name = "<name of your public key>"
 
   tags = {
     Name        = "the-name-of-the-instance"
